3ecb3fbd94edb1d6be44a04b35a98f4ab8af5f66
Charindu Thisara <thisaraimc@gmail.com>
2025-02-12 19:48:18 +0530

    Uncomment test and vet steps in GitHub Actions workflow; set RemoveLimitsDefault to false; adjust CPU limit removal logic in boostContainerResources function

--- internal/webhook/podcpuboost_webhook.go
+++ internal/webhook/podcpuboost_webhook.go
@@ -95,9 +95,9 @@ func (h *podCPUBoostHandler) boostContainerResources(ctx context.Context, b boos
 			"newCpuRequests", resources.Requests.Cpu().String(),
 			"newCpuLimits", resources.Limits.Cpu().String(),
 		)
-		// if h.removeLimits {
-		// 	delete(resources.Limits, corev1.ResourceCPU)
-		// }
+		if h.removeLimits {
+			delete(resources.Limits, corev1.ResourceCPU)
+		}
 		pod.Spec.Containers[i].Resources = *resources
 		log.Info("pod resources increased")
 	}
